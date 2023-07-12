4d0dfbfb2f720772d0dbc24ed6377fcabbdc0c9b
RDiarte <ricardodiarte@gmail.com>
2023-07-12 15:07:13 -0300

    agrego bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import Titles from "./components/Titles";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Titles></Titles>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Titles.jsx
@@ -0,0 +1,10 @@
+const Titles = () => {
+  return (
+    <section className="text-center">
+      <h1 className="display-4">LISTA DE TAREAS</h1>
+      <hr />
+    </section>
+  );
+};
+
+export default Titles;
