01f1c2d0cac88ff45752088b043b9d61f649daf9
RDiarte <ricardodiarte@gmail.com>
2023-07-19 16:41:25 -0300

    creamos Branch crearTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,21 @@
 import { Button, Form } from "react-bootstrap";
 import Lista from "./lista";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
   return (
     <>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            value={tarea}
+            onChange={(e) => setTarea(e.target.value)}
+          />
           <Button variant="primary">Agregar</Button>
         </Form.Group>
       </Form>
